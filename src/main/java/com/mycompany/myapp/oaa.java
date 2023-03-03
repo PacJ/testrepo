0555c4dc565dc8146f6cf4f0a4594907524696ce
viperact <viperact26@gmail.com>
2023-03-03 10:18:53 +0900

    2023-03-03 | JCS

--- src/main/java/com/mycompany/myapp/oaa.java
+++ src/main/java/com/mycompany/myapp/oaa.java
@@ -1,9 +1,7 @@
 package com.mycompany.myapp;
 import java.io.StringWriter;
-import org.python.util.PythonInterpreter;
 public class oaa {
 	public static void main(String[] args) {
-		System.setProperty("python.import.site","false");
 
 	}
 
